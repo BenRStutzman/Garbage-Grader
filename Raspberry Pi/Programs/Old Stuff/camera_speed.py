d95e1cc78cc0929918d338a7fefae64ec606562e
BenRStutzman <tysorx89@gmail.com>
2019-12-01 15:55:52 -0500

    reorganize

--- Raspberry Pi/Programs/Old Stuff/camera_speed.py
+++
@@ -1,4 +0,0 @@
-import os
-
-while True:
-    os.system("raspistill -o test.jpg -t 1 -ss 10000")
--- Raspberry Pi/Programs/Old Stuff/garbage_collector.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-
-while True:
-    files = sorted([f.path for f in os.scandir('photostream')])
-    for file in files[:-10]:
-        os.remove(file)
-    time.sleep(1)
-    
